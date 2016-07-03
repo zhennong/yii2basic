925b4ac5cb381718ee91cf5a512a37cfbc86280a
wodrow <wodrow451611cv@gmail.com>
2016-07-03 18:37:26 +0800

    update

--- views/layouts/left.php
+++ views/layouts/left.php
@@ -7,9 +7,6 @@
                 'options' => ['class' => 'sidebar-menu'],
                 'items' => [
                     ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
-                    ['label' => 'Gii', 'icon' => 'fa fa-file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/debug']],
-                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
                     [
                         'label' => 'Same tools',
                         'icon' => 'fa fa-share',
