7077ed42638887f179b9c925bfba5c3bc3ba5a49
wodrow <wodrow451611cv@gmail.com>
2016-07-09 18:37:20 +0800

    update

---
+++ modules/activity/models/Active.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\modules\activity\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%active}}".
+ *
+ * @property string $id
+ * @property string $title
+ * @property integer $zhekou
+ * @property string $starttime
+ * @property string $endtime
+ * @property string $addtime
+ * @property string $description
+ * @property integer $forcebegin
+ * @property integer $changed
+ */
+class Active extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%active}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'zhekou', 'starttime', 'endtime', 'addtime', 'description', 'forcebegin', 'changed'], 'required'],
+            [['zhekou', 'forcebegin', 'changed'], 'integer'],
+            [['title', 'description'], 'string', 'max' => 255],
+            [['starttime', 'endtime', 'addtime'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => '活动ID',
+            'title' => '活动标题',
+            'zhekou' => '活动折扣',
+            'starttime' => '活动开始时间',
+            'endtime' => '活动结束时间',
+            'addtime' => '活动添加时间',
+            'description' => '活动简述',
+            'forcebegin' => '是否强制开始',
+            'changed' => '是否已经调价',
+        ];
+    }
+}
